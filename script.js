42f204433a6c0d9f7cfc2ca856c2bff6e78446f9
unknown <rodrigoestevan08@gmail.com>
2018-09-30 00:03:15 -0300

    adicionado botao interativo do menu meio

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const botaoMenu = document.querySelector('#burger');
 const menuFlutuante = document.querySelector('#navegacao');
+const botaoMeio = document.querySelector('.sd');
+const secaoMeio = document.querySelector('.meio');
 
 
 
@@ -14,4 +16,14 @@ function controlarMenu(){
       }
     }
 
+    function secaoDoMeio(){
+      if(secaoMeio.style.display === "flex"){
+        secaoMeio.style.display = "";
+      }
+      else{
+        secaoMeio.style.display = "flex"
+      }
+    }
+
     botaoMenu.onclick = controlarMenu;
+    botaoMeio.onclick = secaoDoMeio;
